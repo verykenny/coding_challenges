459908a04601ac4294db21b9de1cc9118b7b6afe
verykenny <16845292+verykenny@users.noreply.github.com>
2021-10-11 16:44:12 -0600

    updated tests

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,3 +1,36 @@
+/*
+
+
+Write a method, digitalRoot(num). It should sum the digits of a positive integer.
+If it is greater than or equal to 10, sum the digits of the resulting number.
+Keep repeating until there is only one digit in the result, called the "digital root".
+Do not use string conversion within your method.
+
+
+13 -> 4
+59 -> 14 -> 5
+132 -> 6
+
+PseudoCode:
+	function(num)
+		if num < 10:
+			return num;
+
+		num = addNums(num)
+		while num > 10:
+			num = addNums(num)
+		return num;
+
+
+	addNums(num)
+		if num > 10:
+			return addNums(num // 10) + (num % 10)
+		return num;
+
+*/
+
+
+
 function digitalRoot(num) {
 	if (num < 10) {
 		return num;
@@ -20,4 +53,4 @@ function addNums(num) {
 
 console.log(digitalRoot(13))
 console.log(digitalRoot(59))
-console.log(digitalRoot(13212346515353216516516513123165651651651351321354526516523216642161351651))
+console.log(digitalRoot(132))
